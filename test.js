8e4b08e375f7150c6897d516b80afff94ff1b0fb
Your Name <you@example.com>
2014-11-19 11:14:55 +0900

    using mocha

--- test.js
+++ test.js
@@ -1,19 +1,47 @@
+var should = require('should');
+
 var Mecab = require('./node-mecab');
 
-Mecab.exec("すもももももももものうち", function (err, result) {
-    if (err) {
-	console.log(err);
-	return;
-    }
-    console.log(result);
+describe('normarl case', function () {
+    it('すもも', function (cb) {
+	var str = "すもももももももものうち";
+	Mecab.exec(str, function (err, result) {
+	    should.not.exist(err);
+	    console.log(result);
+	    (7).should.be.exactly(result.length);
+	    cb();
+	});
+    });
 
+    it('English', function (cb) {
+	Mecab.exec("I am Bob", function (err, result) {
+	    should.not.exist(err);
+	    console.log(result);
+	    (3).should.be.exactly(result.length);
+	    cb();
+	});
+    });
+});
 
-    Mecab.exec("I am Bob", function (err, result) {
-	if (err) {
-	    console.log(err);
-	    return;
-	}
-	console.log(result);
+describe('parseLine', function () {
+    it('', function (cb) {
+	var str = "すもももももももものうち";
+	Mecab.parseLine = function (line) {
+	    var arr = line.split("\t");
+	    if (arr.length < 2) {
+		return null;
+	    }
+	    return arr;
+	};
+	Mecab.exec(str, function (err, result) {
+	    should.not.exist(err);
+	    console.log(result);
+	    (7).should.be.exactly(result.length);
+	    result.forEach(function (array) {
+		(2).should.be.exactly(array.length);
+	    });
+	    cb();
+	});
     });
 });
 
